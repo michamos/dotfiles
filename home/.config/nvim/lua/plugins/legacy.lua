7df0e3482a21c6a475e5a13304ce13a3312a4f03
Micha Moskovic <michamos@gmail.com>
2024-12-10 14:59:41 +0100

    Update LSP config

--- home/.config/nvim/lua/plugins/legacy.lua
+++ home/.config/nvim/lua/plugins/legacy.lua
@@ -1,69 +1,67 @@
 return {
-'tpope/vim-repeat',
-'tpope/vim-surround',
-'tpope/vim-eunuch',
-{ 'tpope/vim-fugitive',
-lazy = false,
-keys = {
-	{ '<Leader>g', vim.cmd.Git },
-},
-init = function() vim.g.fugitive_git_executable = 'LANG=en git' end,  --fugitive is not so good with translations
-},
-'tpope/vim-rhubarb',
-'tpope/vim-git',
-'tpope/vim-sleuth',
-'tpope/vim-speeddating',
-'tpope/vim-afterimage',
-{ 'SirVer/ultisnips' , init = function()
+	'tpope/vim-repeat',
+	'tpope/vim-surround',
+	'tpope/vim-eunuch',
+	{ 'tpope/vim-fugitive',
+		lazy = false,
+		keys = {
+			{ '<Leader>g', vim.cmd.Git },
+		},
+		init = function() vim.g.fugitive_git_executable = 'LANG=en git' end,  --fugitive is not so good with translations
+	},
+	'tpope/vim-rhubarb',
+	'tpope/vim-git',
+	'tpope/vim-sleuth',
+	'tpope/vim-speeddating',
+	'tpope/vim-afterimage',
+	{ 'SirVer/ultisnips' , init = function()
 		vim.g.UltiSnipsEditSplit="horizontal"
 		-- use <C-{t,s}> to jump between snippet insertions
 		vim.g.UltiSnipsJumpForwardTrigger="<C-t>"
 		vim.g.UltiSnipsJumpBackwardTrigger="<C-s>"
-end,
-},
-'honza/vim-snippets',
-{ 'mbbill/undotree', keys = {
-	{  '<Leader>u', vim.cmd.UndotreeToggle },
-}
-},
-{ 'lervag/vimtex', ft = 'tex' , init = function()
-	vim.g.tex_flavor='latex'
-	-- remove sub/superscripts replacement with conceal
-	vim.g.tex_conceal='abdgm'
-	vim.g.vimtex_compiler_method='latexrun'
-	-- vim.g.vimtex_latexmk_continuous=0
-	-- vim.g.vimtex_latexmk_background=1
-	-- vim.g.vimtex_latexmk_options="-pdflatex='pdflatex -synctex=1 \\%O \\%S'"
-	vim.g.vimtex_view_method = 'zathura'
-	vim.g.vimtex_imaps_leader='’'
-end,
-},
-{ 'tommcdo/vim-exchange',
-init = function() vim.g.exchange_no_mappings = true end,
-keys = {
-	{'lx', '<Plug>(Exchange)', mode = 'n'},
-	{'X', '<Plug>(Exchange)', mode = 'x'},
-	{'lxc', '<Plug>(ExchangeClear)', mode = 'n'},
-	{'lxx', '<Plug>(ExchangeLine)', mode = 'n'},
-}
-},
-'chrisbra/csv.vim',
-'michamos/vim-bepo',
-'davidhalter/jedi-vim',
-'hynek/vim-python-pep8-indent',
-'jmcantrell/vim-virtualenv',
-'ledger/vim-ledger',
-'vim-scripts/matchit.zip',
-'leafgarland/typescript-vim',
-{ 'diepm/vim-rest-console' ,
-init = function()
-	vim.g.vrc_set_default_mapping = 0
-	vim.g.vrc_curl_opts = {
-		 ['-sSi'] = '',
+	end,
+	},
+	'honza/vim-snippets',
+	{ 'mbbill/undotree', keys = {
+		{  '<Leader>u', vim.cmd.UndotreeToggle },
+	}
+	},
+	{ 'lervag/vimtex', ft = 'tex' , init = function()
+		vim.g.tex_flavor='latex'
+		-- remove sub/superscripts replacement with conceal
+		vim.g.tex_conceal='abdgm'
+		vim.g.vimtex_compiler_method='latexrun'
+		-- vim.g.vimtex_latexmk_continuous=0
+		-- vim.g.vimtex_latexmk_background=1
+		-- vim.g.vimtex_latexmk_options="-pdflatex='pdflatex -synctex=1 \\%O \\%S'"
+		vim.g.vimtex_view_method = 'zathura'
+		vim.g.vimtex_imaps_leader='’'
+	end,
+	},
+	{ 'tommcdo/vim-exchange',
+		init = function() vim.g.exchange_no_mappings = true end,
+		keys = {
+			{'lx', '<Plug>(Exchange)', mode = 'n'},
+			{'X', '<Plug>(Exchange)', mode = 'x'},
+			{'lxc', '<Plug>(ExchangeClear)', mode = 'n'},
+			{'lxx', '<Plug>(ExchangeLine)', mode = 'n'},
 		}
-	vim.api.nvim_create_user_command('HTTPRequest', 'call VrcQuery()', {})
-end,
-ft = 'rest',
-},
-'tsandall/vim-rego',
+	},
+	'chrisbra/csv.vim',
+	'michamos/vim-bepo',
+	'hynek/vim-python-pep8-indent',
+	'ledger/vim-ledger',
+	'vim-scripts/matchit.zip',
+	'leafgarland/typescript-vim',
+	{ 'diepm/vim-rest-console' ,
+		init = function()
+			vim.g.vrc_set_default_mapping = 0
+			vim.g.vrc_curl_opts = {
+				['-sSi'] = '',
+			}
+			vim.api.nvim_create_user_command('HTTPRequest', 'call VrcQuery()', {})
+		end,
+		ft = 'rest',
+	},
+	'tsandall/vim-rego',
 }
--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,57 @@
-return {
-	"williamboman/mason.nvim",
-	{ 
-		"williamboman/mason-lspconfig.nvim",
-		opts = {
-			ensure_installed = { "pyright", "ruff", "lua_ls" }
-		}
-	},
-	"neovim/nvim-lspconfig",
-}
+local function lua_ls_on_init(client)
+	if client.workspace_folders then
+		local path = client.workspace_folders[1].name
+		if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+			return
+		end
+	end
 
+	client.config.settings.Lua = {
+		runtime = {
+			-- Tell the language server which version of Lua you're using
+			-- (most likely LuaJIT in the case of Neovim)
+			version = 'LuaJIT'
+		},
+		-- Make the server aware of Neovim runtime files
+		workspace = {
+			checkThirdParty = false,
+			library = {
+				vim.env.VIMRUNTIME
+				-- Depending on the usage, you might want to add additional paths here.
+				-- "${3rd}/luv/library"
+				-- "${3rd}/busted/library",
+			}
+			-- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+			-- library = vim.api.nvim_get_runtime_file("", true)
+		}
+	}
+end
+
+local augroup = vim.api.nvim_create_augroup('lsp', { clear = true })
+vim.api.nvim_create_autocmd({'LspAttach'}, {
+  group = augroup,
+  callback = function()
+	vim.keymap.set('n', 'S', vim.lsp.buf.hover, { buffer = true })
+  end,
+  desc = 'Define LSP-powered mappings',
+})
+
+return {
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup {
+				ensure_installed = { "pyright", "ruff", "lua_ls" },
+			}
+			local lsp = require("lspconfig")
+			lsp.pyright.setup {}
+			lsp.ruff.setup {}
+			lsp.lua_ls.setup { on_init = lua_ls_on_init }
+		end,
+	},
+}
